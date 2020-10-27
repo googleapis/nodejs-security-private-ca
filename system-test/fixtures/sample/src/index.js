0a1bb901ee44683e6d6f4020f7483d8ea3739cd9
yoshi-automation <yoshi-automation@google.com>
2020-10-27 03:18:39 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -17,7 +17,7 @@
 // ** All changes to this file may be overwritten. **
 
 /* eslint-disable node/no-missing-require, no-unused-vars */
-const privateca = require('@google-cloud/security-private-ca');
+const privateca = require('privateca');
 
 function main() {
   const certificateAuthorityServiceClient = new privateca.CertificateAuthorityServiceClient();
